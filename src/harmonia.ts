2d0dcb1fb24e7ec3231d77a98070f80e40f722d3
Henrique Mouta <henrique.mouta@automattic.com>
2022-05-03 17:05:02 +0100

    Add Tracks analytics

--- src/harmonia.ts
+++ src/harmonia.ts
@@ -1,12 +1,14 @@
+import stripAnsi from 'strip-ansi';
+
 import Store from './lib/stores/store';
 import SiteConfig from './lib/configs/site.config';
 import EnvironmentVariables from './lib/configs/envvars.config';
 import Test from './lib/tests/test';
 import TestResult, { TestResultType } from './lib/results/testresult';
 import eventEmitter from './lib/events';
-import stripAnsi from 'strip-ansi';
 import { cleanUp } from './utils/shell';
 import Issue from './lib/issue';
+import * as Analytics from './utils/analytics';
 
 /**
  * Test imports
@@ -16,6 +18,7 @@ import TestSuite from './lib/tests/testsuite';
 import DockerSuite from './tests/docker/suite';
 import HealthSuite from './tests/health/suite';
 import TestSuiteResult from './lib/results/testsuiteresult';
+import chalk from 'chalk';
 
 const log = require( 'debug' )( 'harmonia' );
 
@@ -47,6 +50,9 @@ export default class Harmonia {
 			.add( 'env', envVars );
 
 		this.setupTests();
+
+		this.setupAnalytics();
+
 		log( 'Harmonia bootstrap finished' );
 		this.emit( 'harmonia:ready', this );
 	}
@@ -54,6 +60,7 @@ export default class Harmonia {
 	public async run(): Promise< TestResult[] > {
 		this.emit( 'startingTests' );
 		log( 'Starting the tests execution' );
+
 		for ( const test of this.tests ) {
 			// Execute the test
 			const testResult: TestResult = await test.execute();
@@ -66,6 +73,12 @@ export default class Harmonia {
 		}
 		this.emit( 'testsDone', this.results() );
 		log( 'All tests have been executed' );
+
+		// Send analytics event
+		await Analytics.trackEvent( 'run_finished', {
+			total_tests: this.results( false ).length,
+			...this.countResults(),
+		} );
 
 		this.shutdown();
 
@@ -90,6 +103,16 @@ export default class Harmonia {
 			results.push( test.result() );
 			return results;
 		}, [] );
+	}
+
+	public countResults( includeSuites: boolean = false ): {} {
+		return this.results( includeSuites ).reduce( ( counter: object, result: TestResult ) => {
+			if ( ! counter[ result.getTypeString() ] ) {
+				counter[ result.getTypeString() ] = 0;
+			}
+			counter[ result.getTypeString() ]++;
+			return counter;
+		}, { } );
 	}
 
 	public allIssues(): Issue[] {
@@ -155,6 +178,32 @@ export default class Harmonia {
 		log( `Registering test ${ test.name } (${ test.constructor.name })` );
 		test.setOptions( this.options );	// Set the test options
 		this.tests.push( test );			// Store the test
+	}
+
+	private setupAnalytics() {
+		// Setup base configurations
+		Analytics.setUser( '0', 'anon' );
+		Analytics.setBaseParams( { this_is_a_test: 'test' } );
+
+		// Send analytics event on test aborted
+		this.on( 'testAborted', async ( test, testResult ) => {
+			await Analytics.trackEvent( 'test_aborted', {
+				test: stripAnsi( test.constructor.name ),
+				reason: stripAnsi( testResult.getLastIssue().message ),
+			} );
+		} );
+
+		// Send analytics after each test
+		this.on( 'afterTest', async ( test: Test, result: TestResult ) => {
+			const data = {
+				test: test.constructor.name,
+				result: result.getTypeString(),
+				num_errors: result.getErrors().length,
+				num_warnings: result.getWarnings().length,
+				num_blockers: result.getBlockers().length,
+			};
+			await Analytics.trackEvent( 'run_test', data );
+		} );
 	}
 
 	/**
---
+++ src/utils/analytics.ts
@@ -0,0 +1,158 @@
+import { URLSearchParams } from 'url';
+import fetch, { Response } from 'node-fetch';
+import snakeCase from 'lodash.snakecase';
+
+const debug = require( 'debug' )( 'analytics' );
+const validEventOrPropNamePattern = /^[a-z_][a-z0-9_]*$/;
+
+class Analytics {
+	private static instance: Analytics;
+
+	private eventPrefix: string;
+	private userAgent: string = 'vip-harmonia-cli';
+
+	private baseParams: {};
+
+	private readonly ENDPOINT = 'https://public-api.wordpress.com/rest/v1.1/tracks/record';
+
+	constructor( eventPrefix ) {
+		this.eventPrefix = eventPrefix;
+
+		this.baseParams = {
+			'commonProps[_ui]': '0', // TODO: replace with the user id, if running from CLI
+			'commonProps[_ut]': 'anon', // TODO: replace with user type
+			'commonProps[_via_ua]': this.userAgent,
+		};
+	}
+
+	setUser( userId: string, userType: string ) {
+		this.baseParams[ 'commonProps[_ui]' ] = userId;
+		this.baseParams[ 'commonProps[_ut]' ] = userType;
+	}
+
+	setBaseParams( params: {} ) {
+		this.baseParams = { ...this.baseParams, ...params };
+	}
+
+	static getInstance(): Analytics {
+		if ( ! Analytics.instance ) {
+			Analytics.instance = new Analytics( 'vip_harmonia_' );
+		}
+		return Analytics.instance;
+	}
+
+	private sanitizeName( currentName: string ): string {
+		currentName = currentName.replace( ':', '_' );
+		return snakeCase( currentName );
+	}
+
+	async trackEvent( name: string, eventProps = {} ): Promise<Response|boolean|string> {
+		if ( ! name.startsWith( this.eventPrefix ) ) {
+			name = this.eventPrefix + name;
+		}
+
+		if ( ! validEventOrPropNamePattern.test( name ) ) {
+			name = this.sanitizeName( name );
+			if ( ! validEventOrPropNamePattern.test( name ) ) {
+				debug( `Error: Invalid event name detected: ${ name } -- this event will be rejected during ETL` );
+			}
+		}
+
+		// Create and sanitize event props
+		const props = {};
+		Object.keys( eventProps ).forEach( propName => {
+			if ( validEventOrPropNamePattern.test( propName ) ) {
+				props[ propName ] = eventProps[ propName ];
+				return;
+			}
+
+			const sanitezedPropName = this.sanitizeName( propName );
+			if ( validEventOrPropNamePattern.test( sanitezedPropName ) ) {
+				props[ sanitezedPropName ] = eventProps [ propName ];
+				return;
+			}
+
+			debug( `Error: Invalid prop name detected: ${ propName } -- this event will be rejected during ETL` );
+		} );
+
+		const event = Object.assign( {
+			_en: name,
+		}, props );
+
+		// For when we want to support batched events
+		const events = [ event ];
+
+		/**
+		 * The API expects an indexed events array with event data.
+		 *
+		 * `querystring.stringify` does not handle nested arrays and objects very well.
+		 *
+		 * So we can just do it ourselves instead.
+		 *
+		 * Should end up with something like:
+		 *  - events[0][_en]=clickButton // event name
+		 *  - events[0][buttonName]=Deploy // event custom prop
+		 *  - events[1][_en]=loadPage
+		 */
+		const params = events.reduce( ( reduced, ev, index ) => {
+			Object.keys( ev ).forEach( key => {
+				const param = `events[${ index }][${ key }]`;
+				reduced[ param ] = event[ key ];
+			} );
+
+			return reduced;
+		}, {} );
+
+		debug( 'trackEvent()', params );
+
+		try {
+			return await this.send( params );
+		} catch ( error ) {
+			debug( error );
+		}
+
+		// Resolve to false instead of rejecting
+		return Promise.resolve( false );
+	}
+
+	send( extraParams: {} ): Promise<any> {
+		if ( process.env.DO_NOT_TRACK ) {
+			debug( 'send() => skipping per DO_NOT_TRACK variable' );
+
+			return Promise.resolve( 'tracks disabled per DO_NOT_TRACK variable' );
+		}
+
+		const params = Object.assign( {}, this.baseParams, extraParams );
+
+		const method = 'POST';
+		const body = new URLSearchParams( params ).toString();
+		const headers = {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'User-Agent': this.userAgent,
+		};
+
+		debug( 'send()', body );
+
+		// eslint-disable-next-line no-undef
+		return fetch( this.ENDPOINT, {
+			method,
+			body,
+			headers,
+		} );
+	}
+}
+
+export async function trackEvent( name: string, props = {} ) {
+	const analytics = Analytics.getInstance();
+	return analytics.trackEvent( name, props );
+}
+
+export function setUser( userId, userType ) {
+	const analytics = Analytics.getInstance();
+	analytics.setUser( userId, userType );
+}
+
+export function setBaseParams( params ) {
+	const analytics = Analytics.getInstance();
+	analytics.setBaseParams( params );
+}
