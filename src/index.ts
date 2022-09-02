9b145b2e16275cfb7a5e7be20c7ceff82ff74c20
Henrique Mouta <henrique.mouta@automattic.com>
2022-09-02 17:12:18 +0100

    Export more core classes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export { default as Harmonia } from './harmonia';
 export { default as SiteConfig } from './lib/configs/site.config';
 export { default as EnvironmentVariables } from './lib/configs/envvars.config';
+
+export { default as Test } from './lib/tests/test';
+export { default as TestSuite } from './lib/tests/testsuite';
+export { default as TestSuiteResult } from './lib/results/testsuiteresult';
+export { default as TestResult, TestResultType } from './lib/results/testresult';
+export { default as Issue, IssueType } from './lib/issue';
